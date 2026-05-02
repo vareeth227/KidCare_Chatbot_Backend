66a4366781dc835b7e4cc1e7ddac8215baa02f9a
user <be.penag@duocuc.cl>
2026-05-01 21:59:24 -0400

    feat: estructura inicial MS Chatbot

---
+++ src/main/java/com/kidcare/chatbot_service/ChatbotServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kidcare.chatbot_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbotServiceApplication.class, args);
+	}
+
+}
